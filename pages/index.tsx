d6ec1e6a9d2fb139830322213bfc8f1fbbaac7ac
punkrou404 <punkrou404@gmail.com>
2020-11-21 23:05:33 +0900

    install tailwindcss.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <section>
-        <p>[Your Self Introduction]</p>
+        <p className="text-5xl text-center text-gray-700 dark:text-gray-100">[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
