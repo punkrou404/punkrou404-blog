42aed67ae2d6d87f58e8394823d6ba0b18378e6e
punkrou404 <punkrou404@gmail.com>
2020-11-21 19:26:06 +0900

    Simplified index.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ const App = ({ Component, pageProps }: AppProps) => (
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
+      <title>punkrou404 blog</title>
       <link rel="shortcut icon" href="/favicon.ico" key="shortcutIcon" />
       <link rel="manifest" href="/manifest.json" />
     </Head>
