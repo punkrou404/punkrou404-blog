a3f19b6fe433860cf0991fb28376cd7040e9814f
punkrou404 <punkrou404@gmail.com>
2020-11-21 17:56:27 +0900

    Add manifest.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ const App = ({ Component, pageProps }: AppProps) => (
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
       <link rel="shortcut icon" href="/favicon.ico" key="shortcutIcon" />
-      {/* <link rel="manifest" href="/manifest.json" /> */}
+      <link rel="manifest" href="/manifest.json" />
     </Head>
     <Component {...pageProps} />
   </>
