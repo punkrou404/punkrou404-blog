d07bd4e011d08d61d8b1bc62fdd48074b39272f7
punkrou404 <punkrou404@gmail.com>
2020-11-24 21:51:33 +0900

    MOD: convert PNG to SVG

--- src/components/sns.tsx
+++ src/components/sns.tsx
@@ -1,28 +1,28 @@
 const SNS = () => (
-    <nav className="flex w-full">
+    <nav className="flex w-full h-full">
         <div className="flex-grow">
             <a href="https://github.com/punkrou404" target="_blank">
-                <img src="/images/logo/github.logo.png" />
+                <img className="w-8" src="/images/logo/github.logo.svg" />
             </a>
         </div>
         <div className="flex-grow">
             <a href="https://twitter.com/punkrou404" target="_blank">
-                <img src="/images/logo/twitter.logo.png" />
+                <img className="w-8" src="/images/logo/twitter.logo.svg" />
             </a>
         </div>
         <div className="flex-grow">
             <a href="https://qiita.com/punkrou404" target="_blank">
-                <img src="/images/logo/qiita.logo.png" />
+                <img className="w-8" src="/images/logo/qiita.logo.svg" />
             </a>
         </div>
         <div className="flex-grow">
             <a href="https://zenn.dev/punkrou404" target="_blank">
-                <img src="/images/logo/zenn.logo.png" />
+                <img className="w-8" src="/images/logo/zenn.logo.svg" />
             </a>
         </div>
         <div className="flex-grow">
             <a href="https://sauna-ikitai.com/saunners/26885" target="_blank">
-                <img src="/images/logo/sauna-ikitai.logo.svg" />
+                <img className="w-8" src="/images/logo/sauna-ikitai.logo.svg" />
             </a>
         </div>
     </nav>
