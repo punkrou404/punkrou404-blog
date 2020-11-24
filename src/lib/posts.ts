47dd269e528682246a53d930813a418ac5d38022
punkrou404 <punkrou404@gmail.com>
2020-11-24 20:23:56 +0900

    FIX: excluded  analysis.

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -37,23 +37,26 @@ const getPostData = async (id: string) => {
 const getSortedPostsData = (): Array<PostMetaData> => {
     // /posts/ 配下のファイル名を取得
     const fileNames = fs.readdirSync(postsDirectory);
-    const allPostsData = fileNames.map((fileName) => {
-        // idを取得するために`.md`を削除
-        const id = fileName.replace(/\.md$/, '');
+    const allPostsData = fileNames
+        // `[id].tsx`を解析対象外にする
+        .filter((fileName) => fileName.endsWith('.md'))
+        .map((fileName) => {
+            // idを取得するために`.md`を削除
+            const id = fileName.replace(/\.md$/, '');
 
-        // 文字列としてmarkdown読み込む
-        const fullPath = path.join(postsDirectory, fileName);
-        const fileContents = fs.readFileSync(fullPath, 'utf8');
+            // 文字列としてmarkdown読み込む
+            const fullPath = path.join(postsDirectory, fileName);
+            const fileContents = fs.readFileSync(fullPath, 'utf8');
 
-        // gray-matter を使用してメタデータを取得
-        const matterResult = matter(fileContents);
+            // gray-matter を使用してメタデータを取得
+            const matterResult = matter(fileContents);
 
-        // idとメタデータを返却
-        return {
-            id,
-            ...(matterResult.data as { date: string; title: string }),
-        };
-    });
+            // idとメタデータを返却
+            return {
+                id,
+                ...(matterResult.data as { date: string; title: string }),
+            };
+        });
 
     // 日付でポストをソート
     return allPostsData.sort((a, b) => {
