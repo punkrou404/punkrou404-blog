e7cc88ba2f8989c63a7434c0003f5c3f513ffbd0
punkrou404 <punkrou404@gmail.com>
2020-11-27 20:49:45 +0900

    MOD: maintenance style

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,17 +6,19 @@ import Topics from '~/components/topics';
 const Card = ({ props }: PostMeta) => (
     <Link href={`/posts/${props.id}`} key={props.id}>
         <div className="w-full flex flex-col p-3 text-gray-500">
-            <div className="p-4 active:bg-blue-400 hover:bg-blue-300 bg-white rounded-lg shadow-lg flex-1 flex flex-col">
-                <header className="bg-cover">
-                    <h3 className="mb-4 text-2xl">{props.title}</h3>
-                    <div className="mb-4 text-grey-darker text-sm flex-1">
-                        <Date dateString={props.date} />
-                    </div>
-                    <Topics list={props.topics} title={props.title} />
-                </header>
-                <main className="p-4 flex-1 flex flex-col">
-                    <p>{props.summary}</p>
-                </main>
+            <div className="p-4 active:bg-blue-400 bg-blue-200 hover:bg-blue-300 rounded-lg shadow-lg flex-1 flex flex-col">
+                <div className="p-4 rounded-lg active:bg-gray-400 hover:bg-gray-200 bg-white">
+                    <header className="bg-cover">
+                        <h3 className="mb-4 text-2xl">{props.title}</h3>
+                        <div className="mb-4 text-grey-darker text-sm flex-1">
+                            <Date dateString={props.date} />
+                        </div>
+                        <Topics list={props.topics} title={props.title} />
+                    </header>
+                    <main className="p-4 flex-1 flex flex-col">
+                        <p>{props.summary}</p>
+                    </main>
+                </div>
             </div>
         </div>
     </Link>
