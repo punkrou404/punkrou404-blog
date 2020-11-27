03bb50211f86a6bdd057a9704e4aabe930f170ce
punkrou404 <punkrou404@gmail.com>
2020-11-27 21:21:48 +0900

    ADD: progress bar in loading.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import 'styles/index.css';
 import { AppProps } from 'next/app';
+import NextNprogress from 'nextjs-progressbar';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const App = ({ Component, pageProps }: AppProps) => (
     <div>
+        <NextNprogress color="#29D" startPosition={0.3} stopDelayMs={200} />
         <Component {...pageProps} />
     </div>
 );
