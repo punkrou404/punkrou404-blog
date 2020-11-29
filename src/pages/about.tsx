655d9e0c92a45aec273a982364c560eb230cd2ff
punkrou404 <punkrou404@gmail.com>
2020-11-29 15:38:39 +0900

    MOD: change page About to Index.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,4 @@
+import IconLink from '~/components/icon-link';
 import { NextPage } from 'next';
 import { useBreadcrumb } from '~/lib/use-breadcrumb';
 
@@ -16,7 +17,44 @@ const About: NextPage = () => {
 
     return (
         <>
-            <section>About.</section>
+            <section className="grid grid-cols-6 gap-4 justify-center items-center">
+                <div className="col-start-1 col-span-6 h-full ">{/*padding area.*/}</div>
+                <div className="col-start-3 col-span-1 place-self-center">
+                    <img src="profile.png" />
+                </div>
+                <div className="flex-col col-start-4 col-span-1 place-self-center">
+                    <div className="font-semibold text-xl">Sato Kotaro</div>
+                    <div className="font-semibold text-l">@punkrou404</div>
+                    <div className="font-semibold text-m text-gray-400 pt-6">
+                        frontend, backend, infrastructure, sauna, and more...
+                    </div>
+                </div>
+                <div className="col-start-3 col-span-2 h-full">
+                    <nav className="flex w-full h-full">
+                        <IconLink
+                            href="https://github.com/punkrou404"
+                            data="/images/logo/github.logo.svg"
+                        />
+                        <IconLink
+                            href="https://twitter.com/punkrou404"
+                            data="/images/logo/twitter.logo.svg"
+                        />
+                        <IconLink
+                            href="https://qiita.com/punkrou404"
+                            data="/images/logo/qiita.logo.svg"
+                        />
+                        <IconLink
+                            href="https://zenn.dev/punkrou404"
+                            data="/images/logo/zenn.logo.svg"
+                        />
+                        <IconLink
+                            href="https://sauna-ikitai.com/saunners/26885"
+                            data="/images/logo/sauna-ikitai.logo.svg"
+                        />
+                    </nav>
+                </div>
+                <div className="col-start-1 col-span-6">{/*padding area.*/}</div>
+            </section>
         </>
     );
 };
