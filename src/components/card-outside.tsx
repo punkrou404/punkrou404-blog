b70868ea53063b9382ec5cfa070ccbbc42ca1829
punkrou404 <punkrou404@gmail.com>
2021-03-01 01:16:18 +0900

    MOD: Unify to a simple card

--- src/components/card-outside.tsx
+++ src/components/card-outside.tsx
@@ -6,15 +6,11 @@ type P = {
 };
 
 export const CardOutside = ({ children, action = true }: P): JSX.Element => {
-    const style = action ? 'hover:bg-blue-300 active:bg-blue-500' : '';
+    const style = action ? 'hover:bg-gray-200 active:bg-gray-400' : '';
 
     return (
-        <div className={`w-full flex flex-col p-3 text-gray-500`}>
-            <div
-                className={`cursor-pointer p-2 bg-blue-200 rounded-lg shadow-lg flex-1 flex flex-col ${style}`}
-            >
-                <div className={`p-4 rounded-lg  bg-white`}>{children}</div>
-            </div>
+        <div className="w-full flex-col p-3 text-gray-500">
+            <div className={`p-4 bg-white rounded-lg shadow-lg flex-col ${style}`}>{children}</div>
         </div>
     );
 };
