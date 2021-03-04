2dd63d09d3632025faa5486d707b20c7ff4fdf79
punkrou404 <punkrou404@gmail.com>
2021-03-04 17:33:11 +0900

    MOD: SEO

--- src/components/page-head.tsx
+++ src/components/page-head.tsx
@@ -10,11 +10,12 @@ interface Props {
 
 const PageHead = ({ subtitle, description, image, url }: Props): JSX.Element => {
     const title = `punkrou404 blog - ${subtitle}`;
+    image = `/profile.png`;
     return (
         <Head>
             <title>{title}</title>
             <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
+            {/* <meta property="og:description" content={description} /> */}
             <meta property="og:type" content="blog" />
             <meta property="og:url" content={url} />
             <meta property="og:image" content={image} />
@@ -23,11 +24,11 @@ const PageHead = ({ subtitle, description, image, url }: Props): JSX.Element => 
             <meta name="twitter:site" content={`@punkrou404`} />
             <meta name="twitter:url" content={image} />
             <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
+            {/* <meta name="twitter:description" content={description} /> */}
             <meta name="twitter:image" content={image} />
             <link rel="canonical" href={url} />
-            <link rel="shortcut icon" href={`profile.png`} />
-            <link rel="apple-touch-icon" href={`profile.png`} />
+            <link rel="shortcut icon" href={image} />
+            <link rel="apple-touch-icon" href={image} />
         </Head>
     );
 };
